a5455c44dd00289dd8ef7462768c56db9c9129d7
Timwun <NerdisDev@gmail.com>
2021-05-26 23:00:38 +0200

    Adjust code, docs and add ifttt instructions

--- src/main/kotlin/de/tfr/impf/ifttt/IftttClient.kt
+++ src/main/kotlin/de/tfr/impf/ifttt/IftttClient.kt
@@ -1,29 +1,34 @@
 package de.tfr.impf.ifttt
 
 import de.tfr.impf.config.Config
-import de.tfr.impf.sendgrid.log
-import java.net.HttpURLConnection
+import mu.KotlinLogging
 import java.net.URI
-import java.net.URL
 import java.net.URLEncoder
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+
+val log = KotlinLogging.logger("IftttClient")
 
 class IftttClient {
     fun sendMessage(message: String) {
         val apiKey = Config.iftttApiKey
         val eventName = Config.iftttEventName
 
-        val values = mapOf("value1" to message, "value2" to "https://116117.app/")
+        val values = mapOf("value1" to message)
 
-        val client = HttpClient.newBuilder().build();
-        val request = HttpRequest.newBuilder()
-            .uri(URI.create("https://maker.ifttt.com/trigger/$eventName/with/key/$apiKey"))
-            .POST(formData(values))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .build()
-        client.send(request, HttpResponse.BodyHandlers.ofString());
+        try {
+            val client = HttpClient.newBuilder().build()
+            val request = HttpRequest.newBuilder()
+                .uri(URI.create("https://maker.ifttt.com/trigger/$eventName/with/key/$apiKey"))
+                .POST(formData(values))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .build()
+            client.send(request, HttpResponse.BodyHandlers.ofString())
+        } catch (ex: Exception) {
+            log.error { "Error sending IFTTT notification: $ex" }
+        }
+
         log.info { "Notification via IFTTT sent successfully." }
     }
 }
