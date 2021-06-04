a2f8f9b00786814abbfdb6eed2f1d2eac1d7ee12
Hans-Joachim Kliemeck <h0nIg@users.noreply.github.com>
2021-06-04 21:54:12 +0200

    Update ReportJob.kt

--- src/main/kotlin/de/tfr/impf/ReportJob.kt
+++ src/main/kotlin/de/tfr/impf/ReportJob.kt
@@ -15,7 +15,7 @@ val log = KotlinLogging.logger("ReportJob")
 class ReportJob {
 
     private var driver: WebDriver = createDriver()
-	private var startTime = System.currentTimeMillis()
+    private var startTime = System.currentTimeMillis()
 
     private val locations = Config.locationList()
     private val personAge = Config.personAge
@@ -40,12 +40,12 @@ class ReportJob {
                 log.error(e) { "Failed to check location: $location\n" + e.message }
             }
 
-			if (System.currentTimeMillis() > startTime + (1000 * 60 * 60)) {
-				log.info { "Restarting session" }
-				driver.quit()
-				driver = createDriver()
-				startTime = System.currentTimeMillis()
-			}
+            if (System.currentTimeMillis() > startTime + (1000 * 60 * 60)) {
+                log.info { "Restarting session" }
+                driver.quit()
+                driver = createDriver()
+                startTime = System.currentTimeMillis()
+            }
 
             Thread.sleep(Config.waitingTime())
         }
